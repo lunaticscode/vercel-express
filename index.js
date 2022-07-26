527a053cee35261b21afb1fdcee381463c723b32
insoopark <ispark@estsecurity.com>
2022-07-26 22:09:16 +0900

    env init

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const PORT = 8888;
+
+app.use(express.json());
+app.get("/test", (req, res) => {
+  return res.status(200).json({ result: true });
+});
+
+app.post("/test", (req, res) => {
+  console.log(req.body);
+  return res.status(200).json({ result: true });
+});
+
+app.listen(PORT, () => {
+  console.log(`Express running on ${PORT}`);
+});
