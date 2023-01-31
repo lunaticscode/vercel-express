75e9340fe47d33774deceaa7b2ba184957bce733
ispark <ispark@estsecurity.com>
2023-01-31 16:40:24 +0900

    license dummy api 추가

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const fetch = require("node-fetch");
 const FormData = require("form-data");
 const cors = require("cors");
-const { _makeUserData, _apiDataList } = require("./makeData");
+const { _makeUserData, _apiDataList, _licenseDataList } = require("./makeData");
 const app = express();
 const PORT = 8888;
 
@@ -27,22 +27,35 @@ app.post("/user", (req, res) => {
   return res.status(200).json({ result: resultData });
 });
 
-app.get("/api", (req, res) => {
-  const { pageNumber, pageSize = 10 } = req.query;
-  console.log({ pageNumber, pageSize });
-
-  const content = _apiDataList.slice(
+const paginating = (pageNumber, pageSize, type) => {
+  const mapTypeToDataList = {
+    api: _apiDataList,
+    license: _licenseDataList,
+  };
+  const _dataList = mapTypeToDataList[type];
+  const content = _dataList.slice(
     (pageNumber - 1) * pageSize,
     pageNumber * pageSize
   );
   const isLast =
-    content.length < pageSize || content.length === _apiDataList.length;
-  return res.json({
-    allCnt: _apiDataList.length,
+    content.length < pageSize || content.length === _dataList.length;
+  return {
+    allCnt: _dataList.length,
     contentCnt: content.length,
     content,
     isLast,
-  });
+  };
+};
+
+app.get("/api", (req, res) => {
+  const { pageNumber, pageSize = 10 } = req.query;
+  return res.json(paginating(pageNumber, pageSize, "api"));
+});
+
+app.get("/license", (req, res) => {
+  const { pageNumber, pageSize = 10 } = req.query;
+
+  return res.json(paginating(pageNumber, pageSize, "license"));
 });
 
 app.post("/elice-gmail", async (req, res) => {
--- makeData.js
+++ makeData.js
@@ -14,6 +14,31 @@ const _makeUserData = () => {
   };
 };
 
+const _makeLicenseData = (idx) => {
+  const regTimestamp = faker.date.past();
+  const expiredTimestamp = new Date(
+    regTimestamp.getTime() +
+      3600 * 24 * Math.floor(Math.random() * 100 + 30) * 1000
+  );
+  const updateTimestamp = new Date(
+    regTimestamp.getTime() +
+      1000 * 3600 * 24 * Math.floor(Math.random() * 10 + 20)
+  );
+  return {
+    licenseId: 1000 + idx,
+    licenseKey: faker.datatype.uuid().replace(/-/g, "").slice(0, 16),
+    licenseInfoId: Math.floor(Math.random() * 3) + 1,
+    licenseName: "PREMIUM",
+    permission: '{"maxRequestCount":3000}',
+    expiredTimestamp,
+    regTimestamp,
+    regAccount: faker.internet.email(),
+    updateTimestamp,
+    updateAccount: faker.internet.email(),
+    isActive: 1,
+  };
+};
+
 const _makeApiData = () => {
   const regTimestamp = faker.date.past();
   const expireTimestamp = new Date(
@@ -30,6 +55,13 @@ const _makeApiData = () => {
   };
 };
 
+const _licenseDataList = Array.from({ length: 183 }, (_, idx) =>
+  _makeLicenseData(idx)
+).sort(
+  (a, b) =>
+    new Date(b.regTimestamp).getTime() - new Date(a.regTimestamp).getTime()
+);
+
 const _apiDataList = Array.from({ length: 147 }, () => _makeApiData()).sort(
   (a, b) =>
     new Date(b.regTimestamp).getTime() - new Date(a.regTimestamp).getTime()
@@ -40,4 +72,5 @@ module.exports = {
   _makeUserData,
   _makeApiData,
   _apiDataList,
+  _licenseDataList,
 };
