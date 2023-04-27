e67e3bb7867bc3e1994f6c60ad324a2086a2b894
ispark <ispark@estsecurity.com>
2023-04-27 14:00:51 +0900

    user RO 변경

--- routers/user.router.js
+++ routers/user.router.js
@@ -2,10 +2,10 @@ const express = require("express");
 const router = express.Router();
 
 const dummyUsers = [
-  { id: "A0", name: "제우스" },
-  { id: "A1", name: "헤라클레스" },
-  { id: "A2", name: "헤파이토스" },
-  { id: "A3", name: "아레스" },
+  { id: "A0", name: "제우스", createdAt: new Date("2022-01-01") },
+  { id: "A1", name: "헤라클레스", createdAt: new Date("2022-01-02") },
+  { id: "A2", name: "헤파이토스", createdAt: new Date("2022-01-03") },
+  { id: "A3", name: "아레스", createdAt: new Date("2022-01-04") },
 ];
 
 router.get("/", (req, res) => {
